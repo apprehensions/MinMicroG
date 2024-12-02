fd38b3377274fa7a8129405db5ce006479f9efa1
sewn <sewn@disroot.org>
2024-12-02 22:27:17 +0300

    fix support for APatch

--- src/customize.sh
+++ src/customize.sh
@@ -4,6 +4,7 @@ SKIPUNZIP=1;
 export BOOTMODE MODPATH TMPDIR ZIPFILE ARCH IS64BIT API;
 export MAGISK_VER MAGISK_VER_CODE;
 export KSU KSU_VER KSU_VER_CODE KSU_KERNEL_VER_CODE;
+export APATCH APATCH_VER APATCH_VER_CODE;
 
 unzip -qoj "$ZIPFILE" "META-INF/com/google/android/update-binary" -d "$TMPDIR/";
-"$TMPDIR/update-binary" "" "" "$ZIPFILE";
+sh "$TMPDIR/update-binary" "" "" "$ZIPFILE";
